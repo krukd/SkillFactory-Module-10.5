89328f8b34b8087faa983a929ad92a357cd4face
krukd <krukd87@gmail.com>
2023-05-27 12:37:06 +0200

    Task 1 and 2

--- SkillFactory-Module-10.5/Program.cs
+++ SkillFactory-Module-10.5/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.WriteLine("Enter 1st number");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter 2d number");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                ISum sum = new Calculator();
+                ILogger logger = new Calculator();
+
+                logger.Event("Выполняем сложение двух введенных вами чисел...");
+                Console.Write("Сумма равна: ");
+
+                sum.Sum(number1, number2);
+            }
+            catch (Exception ex)
+            {
+
+                ILogger logger = new Calculator();
+                logger.Error("Something went wrong...");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    public interface ISum
+    {
+        void Sum(int a, int b);
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Calculator : ISum, ILogger
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        void ILogger.Error(string message)
+        {
+           Console.BackgroundColor = ConsoleColor.Red;
+           Console.WriteLine(message);
+        }
+
+        void ILogger.Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
         }
     }
 }
